54c6806eacdd02978d96feea4b823e9c7f13b05b
Mary <122350784+MaryNayram@users.noreply.github.com>
2024-04-01 00:38:41 +0000

    Add files via upload

---
+++ backend/components/footer.php
@@ -0,0 +1,5 @@
+<footer class="footer">
+
+   &copy; copyright @ <?= date('Y'); ?> by <span>GuardianCare360</span> | all rights reserved!
+
+</footer>
---
+++ backend/home.php
@@ -0,0 +1,183 @@
+<?php
+
+include 'components/connect.php';
+
+if(isset($_COOKIE['user_id'])){
+   $user_id = $_COOKIE['user_id'];
+}else{
+   $user_id = '';
+}
+
+$select_likes = $conn->prepare("SELECT * FROM `likes` WHERE user_id = ?");
+$select_likes->execute([$user_id]);
+$total_likes = $select_likes->rowCount();
+
+$select_comments = $conn->prepare("SELECT * FROM `comments` WHERE user_id = ?");
+$select_comments->execute([$user_id]);
+$total_comments = $select_comments->rowCount();
+
+$select_bookmark = $conn->prepare("SELECT * FROM `bookmark` WHERE user_id = ?");
+$select_bookmark->execute([$user_id]);
+$total_bookmarked = $select_bookmark->rowCount();
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>home</title>
+
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+
+<?php include 'components/user_header.php'; ?>
+
+<!-- quick select section starts  -->
+
+<section class="quick-select">
+
+   <h1 class="heading">quick options</h1>
+
+   <div class="box-container">
+
+      <?php
+         if($user_id != ''){
+      ?>
+      <div class="box">
+         <h3 class="title">likes and comments</h3>
+         <p>total likes : <span><?= $total_likes; ?></span></p>
+         <a href="likes.php" class="inline-btn">view likes</a>
+         <p>total comments : <span><?= $total_comments; ?></span></p>
+         <a href="comments.php" class="inline-btn">view comments</a>
+         <p>saved playlist : <span><?= $total_bookmarked; ?></span></p>
+         <a href="bookmark.php" class="inline-btn">view bookmark</a>
+      </div>
+      <?php
+         }else{ 
+      ?>
+      <div class="box" style="text-align: center;">
+         <h3 class="title">please login or register</h3>
+          <div class="flex-btn" style="padding-top: .5rem;">
+            <a href="login.php" class="option-btn">login</a>
+            <a href="register.php" class="option-btn">register</a>
+         </div>
+      </div>
+      <?php
+      }
+      ?>
+
+      <div class="box">
+         <h3 class="title">top categories</h3>
+         <div class="flex">
+            <a href="search_course.php?"><i class="	fa fa-shield"></i><span>CIA</span></a>
+            <a href="#"><i class="fa fa-thumb-tack"></i><span>Threat Landscape</span></a>
+            <a href="#"><i class="	fa fa-unlock-alt"></i><span>Key concepts</span></a>
+            <a href="#"><i class="fas fa-chart-line"></i><span>marketing</span></a>
+            <a href="#"><i class="fas fa-clock"></i><span>Terminologies</span></a>
+            <a href="#"><i class="fa fa-user-secret"></i><span>Patient safety</span></a>
+            <a href="#"><i class="fa fa-vcard"></i><span>Awareness</span></a>
+            <a href="#"><i class="fa fa-warning"></i><span>Ransomware</span></a>
+         </div>
+      </div>
+
+      <div class="box">
+         <h3 class="title">popular topics</h3>
+         <div class="flex">
+            <a href="#"><i class="fa fa-search"></i><span>Phishing Awareness and Prevention</span></a>
+            <a href="#"><i class="fa fa-toggle-on"></i><span>Cybersecurity Fundamentals for Healthcare Professionals</span></a>
+            <a href="#"><i class="fa fa-laptop"></i><span>Securing Physical Devices and Workstations</span></a>
+            <a href="#"><i class="fa fa-user-circle"></i><span>Data Privacy</span></a>
+            <a href="#"><i class="fa fa-spinner"></i><span>Secure Password Practices in Healthcare</span></a>
+            <a href="#"><i class="fa fa-tty"></i><span>Incident Response and Reporting</span></a>
+         </div>
+      </div>
+
+      <div class="box tutor">
+         <h3 class="title">Be a trainer</h3>
+         <p>Do you have passion for cybersecurity? Be a part of this revolution.</p>
+         <a href="admin/register.php" class="inline-btn">Get started here</a>
+      </div>
+
+   </div>
+
+</section>
+
+<!-- quick select section ends -->
+
+<!-- courses section starts  -->
+
+<section class="courses">
+
+   <h1 class="heading">latest courses</h1>
+
+   <div class="box-container">
+
+      <?php
+         $select_courses = $conn->prepare("SELECT * FROM `playlist` WHERE status = ? ORDER BY date DESC LIMIT 6");
+         $select_courses->execute(['active']);
+         if($select_courses->rowCount() > 0){
+            while($fetch_course = $select_courses->fetch(PDO::FETCH_ASSOC)){
+               $course_id = $fetch_course['id'];
+
+               $select_tutor = $conn->prepare("SELECT * FROM `tutors` WHERE id = ?");
+               $select_tutor->execute([$fetch_course['tutor_id']]);
+               $fetch_tutor = $select_tutor->fetch(PDO::FETCH_ASSOC);
+      ?>
+      <div class="box">
+         <div class="tutor">
+            <img src="uploaded_files/<?= $fetch_tutor['image']; ?>" alt="">
+            <div>
+               <h3><?= $fetch_tutor['name']; ?></h3>
+               <span><?= $fetch_course['date']; ?></span>
+            </div>
+         </div>
+         <img src="uploaded_files/<?= $fetch_course['thumb']; ?>" class="thumb" alt="">
+         <h3 class="title"><?= $fetch_course['title']; ?></h3>
+         <a href="playlist.php?get_id=<?= $course_id; ?>" class="inline-btn">view playlist</a>
+      </div>
+      <?php
+         }
+      }else{
+         echo '<p class="empty">no courses added yet!</p>';
+      }
+      ?>
+
+   </div>
+
+   <div class="more-btn">
+      <a href="courses.php" class="inline-option-btn">view more</a>
+   </div>
+
+</section>
+
+<!-- courses section ends -->
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- footer section starts  -->
+<?php include 'components/footer.php'; ?>
+<!-- footer section ends -->
+
+<!-- custom js file link  -->
+<script src="js/script.js"></script>
+   
+</body>
+</html>
